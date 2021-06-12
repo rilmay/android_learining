6d2d558e7bf4ad0ee8836d77e6bc82d2ba12c46e
reeelmay <nikita.1999.slipknot@gmail.com>
2021-06-12 22:59:21 +0300

    dimensions resource

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,6 +9,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -34,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        resourceLayout(savedInstanceState);
+        dimensionsProgrammaticalLayout(savedInstanceState);
     }
     // Метод обработки нажатия на кнопку
     public void sendMessage(View view) {
@@ -69,6 +70,33 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
         }
+    }
+
+    public void dimensionsProgrammaticalLayout(Bundle bundle) {
+        Resources resources = getResources();
+        float textSize = resources.getDimension(R.dimen.text_size);
+        int hMargin = (int)resources.getDimension(R.dimen.horizontal_margin);
+        int vMargin = (int)resources.getDimension(R.dimen.vertical_margin);
+
+        ConstraintLayout constraintLayout = new ConstraintLayout(this);
+
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams
+                (ConstraintLayout.LayoutParams.WRAP_CONTENT , ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+
+        TextView textView = new TextView(this);
+        textView.setText("Hello Android");
+        textView.setBackgroundColor(0XFFEAEAEA);
+        // устанавливаем размер шрифт по ресурсу
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+        // устанавливаем отступы пв соответствии с ресурсами
+        layoutParams.setMargins(hMargin, vMargin, hMargin, vMargin);
+
+        textView.setLayoutParams(layoutParams);
+        constraintLayout.addView(textView);
+
+        setContentView(constraintLayout);
     }
 
     public void resourceLayout(Bundle bundle) {
